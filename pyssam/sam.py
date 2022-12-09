10ca03bc779084319d6762e186bb5c2052161adb
jvwilliams23 <jw144@hw.ac.uk>
2022-12-09 22:01:31 +0000

    [docs] add docstrings to sam and ssm

--- pyssam/sam.py
+++ pyssam/sam.py
@@ -1,5 +1,4 @@
-"""Develops SAM based on landmarks and DRRs extracted from the same CT
-dataset."""
+"""Develops SAM to analyse appearance correlations across a dataset."""
 
 from copy import copy
 
@@ -9,17 +8,44 @@ from . import StatisticalModelBase
 
 
 class SAM(StatisticalModelBase):
-  def __init__(self, appearance):
+  """Create statistical appearance model for a set of samples.
+
+  Parameters
+  ----------
+  appearance : array_like
+      Appearances for landmarks in dataset, with 2 dimensions.
+      First dimension has size equal to the number of samples.
+      Second dimension has size equal to the number of landmarks per sample.
+
+  Examples
+  ========
+  >>> num_samples = 5
+  >>> num_landmarks = 10
+  >>> appearances = np.random.normal(size=(num_samples, num_landmarks, 3))
+  >>> sam = pyssam.SAM(appearances)
+  >>> print(sam.appearance_scale.shape)
+  (5, 10)
+  >>> print(sam.compute_dataset_mean().shape)
+  (10,)
+  """
+
+  def __init__(self, appearance: np.ndarray):
 
     # initialise variables
     self.appearance_base = copy(appearance)
-    appearance_aligned = (
+    _appearance_aligned = (
       self.appearance_base - self.appearance_base.mean(axis=1)[:, np.newaxis]
     )
     self.appearance_scale = (
-      appearance_aligned / appearance_aligned.std(axis=1)[:, np.newaxis]
+      _appearance_aligned / _appearance_aligned.std(axis=1)[:, np.newaxis]
     )
 
-  def compute_dataset_mean(self):
-    """"""
+  def compute_dataset_mean(self) -> np.array:
+    """Average over all samples to produce a column-vector of the mean shape.
+
+    Returns
+    -------
+    mean_columnvector : array_like
+        mean shape of all samples in dataset
+    """
     return np.mean(self.appearance_scale, axis=0)
--- pyssam/ssm.py
+++ pyssam/ssm.py
@@ -1,29 +1,51 @@
-"""Develops SSM of lung lobes based on landmarks provided by the user."""
-
 import numpy as np
 
 from . import StatisticalModelBase
 
 
 class SSM(StatisticalModelBase):
-  def __init__(self, landmarks):
+  """Create statistical shape model for a set of shapes.
+
+  Parameters
+  ----------
+  landmarks : array_like
+      Coordinates for landmarks in dataset, with 3 dimensions.
+      First dimension has size equal to the number of samples.
+      Second dimension has size equal to the number of landmarks per sample.
+      Third dimension has size equal to the number of spatial
+        dimensions occupied by the shapes (e.g. 3D or 2D).
+
+  Examples
+  ========
+  >>> num_samples = 5
+  >>> num_landmarks = 10
+  >>> landmarks = np.random.normal(size=(num_samples, num_landmarks, 3))
+  >>> ssm = pyssam.SSM(landmarks)
+  >>> print(ssm.landmarks_columns_scale.shape)
+  (5, 30)
+  >>> print(ssm.compute_dataset_mean().shape)
+  (30,)
+  """
+
+  def __init__(self, landmarks: np.ndarray):
 
     # align all samples to origin
     self.landmarks = landmarks - landmarks.mean(axis=1)[:, np.newaxis]
-    self.landmarks_columns = self.landmarks.reshape(
+    self._landmarks_columns = self.landmarks.reshape(
       landmarks.shape[0], landmarks.shape[1] * landmarks.shape[2]
     )
 
     self.landmarks_columns_scale = (
-      self.landmarks_columns / self.landmarks_columns.std(axis=1)[:, np.newaxis]
+      self._landmarks_columns
+      / self._landmarks_columns.std(axis=1)[:, np.newaxis]
     )
 
-  def compute_dataset_mean(self):
-    """
-    Args:
-      landmark_files
+  def compute_dataset_mean(self) -> np.array:
+    """Average over all samples to produce a column-vector of the mean shape.
 
-    Returns:
-      mean value of each landmark index to create the mean shape
+    Returns
+    -------
+    mean_columnvector : array_like
+        mean shape of all samples in dataset
     """
     return np.mean(self.landmarks_columns_scale, axis=0)
