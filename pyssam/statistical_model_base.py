ad3f98d707b279db466b9e66f7a41e02496e20ae
jvwilliams23 <jw144@hw.ac.uk>
2022-12-09 17:28:25 +0000

    [docs] add docstrings and type hints to base class

--- pyssam/statistical_model_base.py
+++ pyssam/statistical_model_base.py
@@ -1,19 +1,46 @@
-"""Develops SSM of lung lobes based on landmarks provided by the user."""
 from abc import ABC, abstractmethod, abstractproperty
+from typing import Any, Tuple
 
 import numpy as np
+import sklearn
 from sklearn.decomposition import PCA
 
 
 class StatisticalModelBase(ABC):
+  """Abstract base class for statistical model."""
+
   @abstractmethod
-  def compute_dataset_mean(self):
+  def compute_dataset_mean(self) -> np.array:
+    """Average over all samples to produce a column-vector of the mean shape,
+    appearance, or other quantity included in model.
+
+    Returns
+    -------
+    mean_columnvector : array_like
+    """
     pass
 
-  def create_pca_model(self, dataset_columnvector, desired_variance=0.9):
+  def create_pca_model(
+    self, dataset: np.ndarray, desired_variance: float = 0.9
+  ) -> None:
+    """Perform principal component analysis to create statistical model and
+    extract modelling quantites from the PCA class.
+
+    Parameters
+    ----------
+    dataset : array_like
+        2D array of data to model, where each row on the first axis is one sample
+        and each column on the second axis is e.g. shape or appearance for a landmark
+    desired_variance : float
+        Fraction of total variance to be described by the reduced-dimension model
+
+    Returns
+    -------
+    None
+    """
     # perform principal component analysis to train shape model
-    self.pca_object, self.k = self.do_pca(
-      dataset_columnvector, desired_variance
+    self.pca_object, self.required_mode_number = self.do_pca(
+      dataset, desired_variance
     )
     # get principal components (eigenvectors) and variances (eigenvalues)
     self.pca_model_components = self.pca_object.components_
@@ -26,39 +53,79 @@ class StatisticalModelBase(ABC):
     self.desired_variance = desired_variance
 
   def morph_model(
-    self, mean_dataset_columnvector, pca_model_components, model_parameters
-  ):
-    """Return point cloud that has been adjusted by a specified shape vector
-    (b)
+    self,
+    mean_dataset_columnvector: np.array,
+    pca_model_components: np.ndarray,
+    model_parameters: np.array,
+  ) -> np.array:
+    """Morph the mean dataset based on the PCA weights and variances,
+    with some user-defined model parameters to create a new sample.
 
-    Args:
-      mean_dataset_columnvector (3n,) array: mean shape
-      pca_model_components (sampleNum, 3n) array: PCA components
-      b (sampleNum, ) array: shape vector to vary points by
+    Parameters
+    ----------
+    mean_dataset_columnvector : array_like
+        mean shape of the training data in a 1D array
+    pca_model_components : array_like
+        eigenvectors of covariance matrix, obtain by principal component analysis
+    model_parameters : array_like
+        model parameters used to perturb each principal component by some amount
+        1D array, where values should all be within +/- 3
+
+    Returns
+    -------
+    morphed output : array_like
+        A 1D array which has been perturbed from the mean shape based
+        on the pca_model and model_parameters
+
+    Raises
+    ------
+    Warning
+        If model parameters are outwith +/- 3
+    AssertionError
+        If number of dimension in pca_model_components not equal to 2
     """
+    if np.any(abs(model_parameters) > 3.0):
+      Warning(
+        f"Applying large model parameter ({abs(model_parameters).max()}) "
+        "which may produce unrealistic output"
+      )
+    assert (
+      pca_model_components.ndim == 2
+    ), (
+      f"pca model not of expected number of dimensions" 
+      f" (shape is {pca_model_components.shape})"
+    )
     model_weight = model_parameters * self.std
     return mean_dataset_columnvector + np.dot(
       pca_model_components.T, model_weight
     )
 
-  def do_pca(self, dataset_columnvector, desired_variance=0.95):
-    """
-    Args:
-      dataset_columnvector = 2D array of components
+  def do_pca(self, dataset: np.ndarray, desired_variance: float = 0.9) -> Tuple[Any, int]:
+    """Fit principal component analysis to given dataset.
+
+    Parameters
+    ----------
+    dataset : array_like
+        2D array of data to model, where each row on the first axis is one sample
+        and each column on the second axis is e.g. shape or appearance for a landmark
+    desired_variance : float
+        Fraction of total variance to be described by the reduced-dimension model
 
     Returns:
-      pca, variation of each mode
-      desired_variance, amount of modes to reach desired variance ratio
+    pca : sklearn.decomposition._pca.PCA
+        Object containing fitted PCA information e.g. components, explained variance
+    required_mode_number : int
+        Number of principal components needed to produce desired_variance
     """
     pca = PCA(svd_solver="full")
-    pca.fit(dataset_columnvector)
-    required_num_modes = np.where(
+    pca.fit(dataset)
+    required_mode_number = np.where(
       np.cumsum(pca.explained_variance_ratio_) > desired_variance
     )[0][0]
     print(
-      f"Reduced to {required_num_modes} components "
+      f"Reduced to {required_mode_number} components "
       f"from {len(pca.explained_variance_ratio_)} "
       f"for {desired_variance*100}% variation"
     )
 
-    return pca, required_num_modes
+    return pca, required_mode_number
