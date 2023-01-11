3c36762a8045616effd643024de75bc7535cca0a
jvwilliams23 <jw144@hw.ac.uk>
2023-01-11 08:56:40 +0000

    packaging: update packaging to use pyproject.toml
    As setuptools is depreciated, use pyproject.toml
    by following https://packaging.python.org/en/latest/tutorials/packaging-projects/
    Works with testPyPI and passes all tests.

--- pyssam/__init__.py
+++ pyssam/__init__.py
@@ -3,7 +3,6 @@ from pyssam.statistical_model_base import StatisticalModelBase
 from pyssam.ssm import SSM
 from pyssam.sam import SAM
 from pyssam.ssam import SSAM
-
-from . import datasets
+from pyssam import datasets
 
 __all__ = ["SSM", "SAM", "SSAM", "datasets"]
--- pyssam/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-# To get sub-modules
-from .datasets import *
-
-__all__ = datasets.__all__.copy()
