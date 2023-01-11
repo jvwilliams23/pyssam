daec461db797dd01c3b7950be6ef671627fd2450
jvwilliams23 <jw144@hw.ac.uk>
2023-01-11 17:46:17 +0000

    tests: replace Tree dataset with example lung data
    Tree dataset sometimes had issues with abnormally large number of modes.
    Lung dataset seems more robust.
    Also chose random sample to test each iteration.

--- tests/test_ssm.py
+++ tests/test_ssm.py
@@ -3,24 +3,40 @@ import unittest
 import numpy as np
 import pyssam
 
+def read_lung_data():
+  from glob import glob
+  LANDMARK_DIR = "example_data/lung_landmarks/"
+  landmark_dir_list = glob(f"{LANDMARK_DIR}/landmarks*.csv")
+  landmark_dir_list = sorted(
+    landmark_dir_list, key=lambda x: int(x.replace(".csv", "")[-4:])
+  )
+
+  if len(landmark_dir_list) == 0:
+    raise AssertionError(
+      "ERROR: The directories you have declared are empty.",
+      "\nPlease check your input arguments.",
+    )
+
+  landmark_coordinates = np.array(
+    [np.loadtxt(l, delimiter=",") for l in landmark_dir_list]
+  )
+
+  return landmark_coordinates
 
 class TestSSM(unittest.TestCase):
   def test_morph_model(self):
     num_repititions = 10
     for _ in range(0, num_repititions):
-      num_samples = 50
-      tree_class = pyssam.datasets.Tree(num_extra_ends=1)
-      landmark_coordinates = np.array(
-        [tree_class.make_tree_landmarks() for i in range(0, num_samples)]
-      )
+      landmark_coordinates = read_lung_data()
 
       ssm_obj = pyssam.SSM(landmark_coordinates)
       ssm_obj.create_pca_model(ssm_obj.landmarks_columns_scale)
       mean_shape_columnvector = ssm_obj.compute_dataset_mean()
       mean_shape = mean_shape_columnvector.reshape(-1, 3)
 
+      test_sample_id = np.random.randint(0, len(landmark_coordinates))
       test_shape_columnvec = (
-        landmark_coordinates[0] - landmark_coordinates[0].mean(axis=0)
+        landmark_coordinates[test_sample_id] - landmark_coordinates[test_sample_id].mean(axis=0)
       ).reshape(-1)
       test_shape_columnvec /= test_shape_columnvec.std()
       test_params = ssm_obj.fit_model_parameters(test_shape_columnvec, ssm_obj.pca_model_components)
@@ -49,11 +65,7 @@ class TestSSM(unittest.TestCase):
   def test_morph_model_reduced_dimension(self):
     num_repititions = 10
     for _ in range(0, num_repititions):
-      num_samples = 50
-      tree_class = pyssam.datasets.Tree(num_extra_ends=1)
-      landmark_coordinates = np.array(
-        [tree_class.make_tree_landmarks() for i in range(0, num_samples)]
-      )
+      landmark_coordinates = read_lung_data()
 
       ssm_obj = pyssam.SSM(landmark_coordinates)
       ssm_obj.create_pca_model(ssm_obj.landmarks_columns_scale, desired_variance=0.7)
@@ -61,8 +73,9 @@ class TestSSM(unittest.TestCase):
       mean_shape_columnvector = ssm_obj.compute_dataset_mean()
       mean_shape = mean_shape_columnvector.reshape(-1, 3)
 
+      test_sample_id = np.random.randint(0, len(landmark_coordinates))
       test_shape_columnvec = (
-        landmark_coordinates[0] - landmark_coordinates[0].mean(axis=0)
+        landmark_coordinates[test_sample_id] - landmark_coordinates[test_sample_id].mean(axis=0)
       ).reshape(-1)
       test_shape_columnvec /= test_shape_columnvec.std()
       test_params = ssm_obj.fit_model_parameters(test_shape_columnvec, ssm_obj.pca_model_components)
@@ -88,15 +101,13 @@ class TestSSM(unittest.TestCase):
   def test_fit_model_parameters_all_modes(self):
     num_repititions = 10
     for _ in range(0, num_repititions):
-      num_samples = 50
-      tree_class = pyssam.datasets.Tree(num_extra_ends=1)
-      landmark_coordinates = np.array(
-        [tree_class.make_tree_landmarks() for i in range(0, num_samples)]
-      )
+      landmark_coordinates = read_lung_data()
 
       ssm_obj = pyssam.SSM(landmark_coordinates)
       ssm_obj.create_pca_model(ssm_obj.landmarks_columns_scale, desired_variance=0.7)
-      target_shape = ssm_obj.landmarks_columns_scale[0]
+
+      test_sample_id = np.random.randint(0, len(landmark_coordinates))
+      target_shape = ssm_obj.landmarks_columns_scale[test_sample_id]
       model_parameters = ssm_obj.fit_model_parameters(target_shape, ssm_obj.pca_model_components)
       model_parameters = np.where(model_parameters < 5, model_parameters, 3)
       model_parameters = np.where(model_parameters > -5, model_parameters, -3)
@@ -109,15 +120,13 @@ class TestSSM(unittest.TestCase):
   def test_fit_model_parameters_reduced_modes(self):
     num_repititions = 10
     for _ in range(0, num_repititions):
-      num_samples = 50
-      tree_class = pyssam.datasets.Tree(num_extra_ends=1)
-      landmark_coordinates = np.array(
-        [tree_class.make_tree_landmarks() for i in range(0, num_samples)]
-      )
+      landmark_coordinates = read_lung_data()
 
       ssm_obj = pyssam.SSM(landmark_coordinates)
       ssm_obj.create_pca_model(ssm_obj.landmarks_columns_scale, desired_variance=0.7)
-      target_shape = ssm_obj.landmarks_columns_scale[0]
+
+      test_sample_id = np.random.randint(0, len(landmark_coordinates))
+      target_shape = ssm_obj.landmarks_columns_scale[test_sample_id]
       model_parameters = ssm_obj.fit_model_parameters(target_shape, ssm_obj.pca_model_components, num_modes=2)
       dataset_mean = ssm_obj.compute_dataset_mean()
       morphed_shape = ssm_obj.morph_model(dataset_mean, ssm_obj.pca_model_components, model_parameters, num_modes=2)
@@ -125,11 +134,8 @@ class TestSSM(unittest.TestCase):
       assert not np.isclose(error.mean(), 0), f"error is zero ({error.mean()}), but should be non-zero"
 
   def test_desired_variance_bounds(self):
-    tree_class = pyssam.datasets.Tree(num_extra_ends=0)
-    num_samples = 10
-    landmark_coordinates = np.array(
-      [tree_class.make_tree_landmarks() for i in range(0, num_samples)]
-    )
+    landmark_coordinates = read_lung_data()
+
     ssm_obj = pyssam.SSM(landmark_coordinates)
     desired_variance_list = [-0.0001, -50, 1.1, 1.00001, 50]
     for desired_variance in desired_variance_list:
@@ -143,7 +149,7 @@ class TestSSM(unittest.TestCase):
         AssertionError("AssertionError for desired variance not identified")
 
   def test_property_decorator(self):
-    dataset_ssm = np.ones((10,10,3))
+    dataset_ssm = np.random.normal(size=(10,10,3))
     ssm = pyssam.SSM(dataset_ssm)
     try:
       ssm.num_landmarks = 20
@@ -155,13 +161,13 @@ class TestSSM(unittest.TestCase):
       )
 
   def test__check_data_shape(self):
-    dataset_ssm = np.ones((10,10,3))
-    base_dataset_ssm = np.ones((10,10,3))
+    dataset_ssm = np.random.normal(size=(10,10,3))
+    base_dataset_ssm = np.random.normal(size=(10,10,3))
     assert pyssam.SSM(base_dataset_ssm)._check_data_shape(dataset_ssm).ndim == 3, "ndim not equal to 3"
     dataset_ssm = np.ones((10,10))
     assert pyssam.SSM(base_dataset_ssm)._check_data_shape(dataset_ssm).ndim == 3, "ndim not equal to 3"
     
-    dataset_ssm = np.ones((10))
+    dataset_ssm = np.random.normal(size=(10))
     try:
       pyssam.SSM(base_dataset_ssm)._check_data_shape(dataset_ssm).ndim
     except AssertionError:
@@ -169,7 +175,7 @@ class TestSSM(unittest.TestCase):
     else:
       AssertionError(f"SSM._check_data_shape not recognising input with ndim = {dataset_ssm.ndim}")
 
-    dataset_ssm = np.ones((10)*4)
+    dataset_ssm = np.random.normal(size=(10)*4)
     try:
       pyssam.SSM(base_dataset_ssm)._check_data_shape(dataset_ssm).ndim
     except AssertionError:
