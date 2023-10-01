7721f93bbdb2d81d4680b65717aeba4bbf1239de
jvwilliams23 <josh.williams@stfc.ac.uk>
2023-10-01 19:57:08 +0100

    implement growing phase up-to firing value part

--- scripts/games_landmarking.py
+++ scripts/games_landmarking.py
@@ -1,27 +1,4 @@
-"""
-	USAGE: 
-	python games_master.py -inp landmarksBase.csv edgesBase.csv 
-													-meshdir /home/josh/luna16_labels/
-
-	HELP:
-	python games_master.py --help
-	python games_master.py -h
-
-	Uses GAMEs (Growing and Adaptive Meshes) alignment for 
-	automatic landmarking of SSM.
-	
-	Based on:
-	L. Ferrarini et al, Medical Image Analysis 11 (2007) 302-314
-
-	Programmed by:		Josh Williams
-	Began:				15/03/2020
-
-  two most time consuming are advance t counter and arrayRowIntersection
-
-"""
-
 import argparse
-import re
 from copy import copy
 from glob import glob
 from random import choices
@@ -53,7 +30,6 @@ def euclideanDist(x, y):
     return np.sqrt(np.sum((x - y) ** 2))
   else:
     return np.sqrt(np.sum((x - y) ** 2, axis=1))
-
 
 def mahalanobisDist(x, y):
   """
@@ -114,16 +90,12 @@ def f_n(t_counter, alpha_n=1.05, beta_n=14.3):
 
 
 def grow_landmark_network(
-  surface_points_wholeSurf, case_name, coarsenFactor=4, activation_threshold=0.01, organ=""
+  surface_points_orig, activation_threshold=0.01
 ):
   """
   Args:
-  surface_points_wholeSurf (-1x3 array): Point cloud of entire surface mesh
+  surface_points_orig (-1x3 array): Point cloud of entire surface mesh
   (used for accuracy comparison)
-
-  Returns:
-  A (-1x3 array): list of nodes, representing landmarks of surface model
-  edge_age (-1x6 array): Edges connecting pairs of landmarks
 
   Performs growing phase of the GAMEs algorithm.
   This function performs unsupervised clustering of a set of surface coordinates,
@@ -132,25 +104,21 @@ def grow_landmark_network(
   the subset of surrounding surface points best.
   """
   graph = nx.DiGraph()
+  graph.add_nodes_from([0, 1])
 
   # -Initialise - need to adjust A to remove matching nodes
-  surface_points = shuffle(surface_points_wholeSurf[::coarsenFactor])
+  surface_points = shuffle(surface_points_orig)
   landmark_setA = choices(surface_points, k=2)  # set of nodes
-  if args.debug:
-    landmark_setA = [surface_points[0], surface_points[1000]]
+  # landmark_setA = [surface_points[0], surface_points[1000]]
   landmark_setA = np.array(landmark_setA, dtype="float64")
 
   if np.allclose(landmark_setA[0], landmark_setA[1]):
     exit()
-  edge_age = np.zeros(
-    7
-  )  # [] # empty set of edges and their edges (edge edge age = edge_age)
-  edge_age.shape = (1, 7)
+  graph.nodes[0]["position"] = landmark_setA[0]
+  graph.nodes[1]["position"] = landmark_setA[1]
   threshold_std_dev_dist_to_surf = 0.01  # Threshold accuracy
   threshold_average_dist_to_surf = 5
-  # activation_threshold = 0.001  # 0.025 # activation threshold
   edge_age_threshold = 50  # edge age threshold
-  t_counterList = np.column_stack((surface_points, np.zeros(len(surface_points))))
 
   distance_metric = euclideanDist
 
@@ -164,28 +132,81 @@ def grow_landmark_network(
 
   while not round(average_distance_lm_to_surf, 1) <= threshold_average_dist_to_surf:
     for surface_point_i in surface_points:
-      pass
       # find closest node to surface_point_i
+      graph_positions = _graph_nodes_to_positions(graph)
+      distance_to_point_i = distance_metric(graph_positions, surface_point_i)
+      nearest_point_index = np.argmin(distance_to_point_i)
+      nearest_node = list(graph.nodes)[nearest_point_index]
+      position_nearest_node = graph.nodes[nearest_node]["position"]
+      distance_closest_node = distance_to_point_i[nearest_point_index]
+      # find second closest node
+      distance_to_point_i[np.argmin(distance_to_point_i)] = np.inf
+      second_nearest_point_index = np.argmin(distance_to_point_i)
+      second_nearest_node = list(graph.nodes)[second_nearest_point_index]
 
-      # find best matching node and second best batching node
-
-      # create edge between best and second best matching node
-      # set edge age = 0
+      # create edge between best and second best matching node, set edge age = 0
+      edge_i = (nearest_node, second_nearest_node)
+      # TODO: maybe need to check if edge exists
+      graph.add_edge(*edge_i)
+      graph.edges[edge_i]["age"] = 0
+      
+      # TODO
+      firing_value_nearest = 1
 
       # evaluate activation function for best matching node
-
+      activation_value = np.exp(-distance_closest_node)
       # check if activtion less than cutoff.
-
+      if activation_value < activation_threshold: #TODO: and f_s < f_T
+        # add new node and connect to graph (grow graph)
+        new_node = max(graph.nodes)+1
+        graph.add_node(new_node, position=(surface_point_i+position_nearest_node)/2)
+        graph.add_edge(new_node, nearest_node, age=0)
+        graph.add_edge(new_node, second_nearest_node, age=0)
+        graph.remove_edge(nearest_node, second_nearest_node)
+      else:
+        # adapt nearest node and its neighbors
+        morph_parameter_nearest = 0.1 # e_w in paper
+        # morph_parameter_neighbors = morph_parameter_nearest * np.exp(-) # e_n in paper
+        connected_edges = list(graph.edges(nearest_node))
+        for edge_j in connected_edges:
+          node_j = edge_j[1]
+          firing_value_neighbor = 1 # TODO
+          position_node_j = graph.nodes[node_j]["position"]
+          distance_nearest_to_neighbor = distance_metric(position_nearest_node, position_node_j)
+          SIGMA = 0.42 # empirical parameter from Ferrarini et al.
+          morph_parameter_neighbor_j = morph_parameter_nearest * np.exp(-distance_nearest_to_neighbor / (2 * SIGMA**2.0))
+          # move neighbor nodes position
+          graph.nodes[node_j]["position"] += morph_parameter_neighbor_j * firing_value_neighbor * (surface_point_i - position_node_j)
+        # move nearest node position
+        graph.nodes[nearest_node]["position"] += morph_parameter_neighbor_j * firing_value_neighbor * (surface_point_i - position_node_j)
       # increase age of edges ENDING in best matching node (implies we need a DiGraph)
+      neighbor_edges = graph.out_edges(nearest_node)
+      for edge_i in neighbor_edges:
+        graph.edges[edge_i]["age"] += 1
 
-      # decrease diring value of best matching node and its neighbors
+      # decrease firing value of best matching node and its neighbors
 
       # remove edges where age is above a cutoff
+      graph = _check_remove_edge_age(graph)
 
       # remove isolated nodes
+      graph = graph.remove_nodes_from(nx.isolates(graph))
+      continue
 
   return graph
 
+def _check_remove_edge_age(graph, age_cutoff=50):
+  remove_edges = []
+  for edge_i in graph.edges:
+    age = graph.edges[edge_i]["age"]
+    if age > age_cutoff:
+      remove_edges.append(edge_i)
+
+  graph.remove_edges_from(remove_edges)
+  return graph
+
+def _graph_nodes_to_positions(graph):
+  return np.array(list(nx.get_node_attributes(graph, "position").values()))
 
 ###################################################################################
 # 							MAIN RUN FOR ADAPT CODE
