8a67bb299cfa50460681eece82f5084b068254b0
jvwilliams23 <josh.williams@stfc.ac.uk>
2023-12-06 09:20:27 +0000

    add unit test for torus dataset check number of modes for scaled and unscaled version. check type for data.points()

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -15,6 +15,57 @@ class TestDatasets(unittest.TestCase):
         num_ends == expected_ends
       ), f"expected landmarks {expected_ends} != {num_ends}"
 
+  def test_torus_modes_normalised(self):
+    N_SAMPLES = 100
+
+    torus = pyssam.datasets.Torus()
+    data = torus.make_dataset(N_SAMPLES)
+
+    landmark_coordinates = np.array(
+      [sample_i.points()[::10] for sample_i in data]
+    )
+
+    # when landmarks are scaled, should only be one mode (inner/outer radius ratio)
+    EXPECTED_NONZERO_MODES = 1
+    ssm_obj = pyssam.SSM(landmark_coordinates)
+    ssm_obj.create_pca_model(ssm_obj.landmarks_columns_scale)
+    explained_variance = ssm_obj.pca_object.explained_variance_ratio_
+    num_nonzero_modes = (explained_variance > 0.1).sum()
+    assert (
+      num_nonzero_modes == EXPECTED_NONZERO_MODES
+    ), f"num non-zero modes {num_nonzero_modes} but expected {EXPECTED_NONZERO_MODES} for variances {explained_variance}"
+
+  def test_torus_modes_not_normalised(self):
+    N_SAMPLES = 100
+
+    torus = pyssam.datasets.Torus()
+    data = torus.make_dataset(N_SAMPLES)
+
+    landmark_coordinates = np.array(
+      [sample_i.points()[::10] for sample_i in data]
+    )
+
+    # when landmarks are not scaled, should be two modes (inner radius, outer radius)
+    EXPECTED_NONZERO_MODES = 2
+    ssm_obj = pyssam.SSM(landmark_coordinates)
+    ssm_obj.create_pca_model(landmark_coordinates.reshape(N_SAMPLES, -1))
+    explained_variance = ssm_obj.pca_object.explained_variance_ratio_
+    num_nonzero_modes = (explained_variance > 0.1).sum()
+    assert (
+      num_nonzero_modes == EXPECTED_NONZERO_MODES
+    ), f"num non-zero modes {num_nonzero_modes} but expected {EXPECTED_NONZERO_MODES} for variances {explained_variance}"
+
+  def test_torus_to_points(self):
+    N_SAMPLES = 2
+
+    torus = pyssam.datasets.Torus()
+    data = torus.make_dataset(N_SAMPLES)
+
+    # landmark_coordinates = np.array(
+    #   [sample_i.points()[::10] for sample_i in data]
+    # )
+    assert type(data[0].points()) == np.ndarray, f"torus.points() type np.ndarray != {type(data[0].points())}"
+
 
 if __name__ == "__main__":
   unittest.main()
